4ba4d0f354d10ad25e0c7e982f2432209c7290f1
junaid <merchantjs@gmail.com>
2019-11-08 02:01:30 -0500

    updated check column 5

--- fMRI/fx/motion/auto-motion-fmriprep/rp_txt/CheckColumn5.sh
+++ fMRI/fx/motion/auto-motion-fmriprep/rp_txt/CheckColumn5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for s in $(ls rp*.txt); do
 test=`cat $s | awk '{sum+=$5} END{print sum}'`
-if [ test -eq 0 ]; then
+if [ $test -eq 0 ]; then
 echo $s
 cat $s | awk '{print $1,$2,$3,$4}' > FourColumn/${s}
 fi
